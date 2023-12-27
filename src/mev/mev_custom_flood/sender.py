b172f16263fc62ee84384bbee9cf7f17dcb943cb
Kamil Chodoła <kamil@nethermind.io>
2023-12-27 14:02:56 +0100

    Add draft tx verifier

--- src/mev/mev_custom_flood/sender.py
+++ src/mev/mev_custom_flood/sender.py
@@ -10,8 +10,10 @@ import os
 import time
 import logging
 import click
+import threading
 
-
+# Global variables
+TX_HASHES = []  # List to store transaction hashes
 VALUE_TO_SEND = 0x9184
 
 logging.basicConfig(filename="/tmp/sender.log",
@@ -28,6 +30,8 @@ EL_URI = os.getenv("EL_RPC_URI", 'http://0.0.0.0:53913')
 
 
 def send_transaction():
+    global TX_HASHES
+
     # Setting w3 as constant causes recursion exceeded error after ~500 transactions
     # Thus it's created everytime a transaction is sent
     w3 = Web3(Web3.HTTPProvider(EL_URI))
@@ -53,16 +57,57 @@ def send_transaction():
     logging.info(tx_hash.hex())
     assert tx["from"] == sender_account.address
 
+    tx_hashes.append(tx_hash.hex())  # Add tx_hash to the global list
+
+def verify_transaction(tx_hash):
+    w3 = Web3(Web3.HTTPProvider(EL_URI))
+
+    # Check for transaction receipt
+    receipt = w3.eth.get_transaction_receipt(tx_hash)
+    if receipt:
+        logging.info(f"Transaction receipt found. Block Number: {receipt.blockNumber}")
+    else:
+        logging.warning(f"No receipt found for transaction (TX Hash: {tx_hash})")
+
+
+def check_receipts_thread():
+    global tx_hashes
+
+    w3 = Web3(Web3.HTTPProvider(EL_URI))
+
+    while True:
+        current_time = time.time()
+        for tx_hash, timestamp in list(tx_hashes):
+            if current_time - timestamp > 60:  # Check if 60 seconds have passed
+                logging.error(f"Transaction receipt not found within 60 seconds for {tx_hash}")
+                tx_hashes.remove((tx_hash, timestamp))  # Remove the hash if timeout
+                continue
+
+            try:
+                receipt = w3.eth.get_transaction_receipt(tx_hash)
+                if receipt:
+                    logging.info(f"Transaction receipt found. Block Number: {receipt.blockNumber}")
+                    tx_hashes.remove((tx_hash, timestamp))  # Remove the hash after checking
+            except Exception as e:
+                # If receipt is not yet available, do nothing
+                pass
+
+        time.sleep(1)  # Short delay to prevent excessive CPU usage
 
 def delayed_send(interval_between_transactions):
-    send_transaction()
-    time.sleep(interval_between_transactions)
+    while True:
+        tx_hash = send_transaction()
+        time.sleep(interval_between_transactions)
 
 
 @click.command()
 @click.option('--interval_between_transactions', default=0.5, help='Interval between successive transaction sends (in seconds). The value may be an integer or decimal')
 def run_infinitely(interval_between_transactions):
     logging.info(f"Using sender {SENDER} receiver {RECEIVER} and el_uri {EL_URI}")
+
+    # Start the transaction verification thread
+    threading.Thread(target=check_receipts_thread, daemon=True).start()
+
     spam = send_transaction if interval_between_transactions == 0 else partial(delayed_send, interval_between_transactions)
     while True:
         try:
